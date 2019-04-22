1b1ca0612d170f93d6f5da21468c1239a1ffa656
Zach Hardesty <zachhardesty7@gmail.com>
2019-04-22 13:42:59 -0500

    add FB auth testing

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,12 +1,15 @@
-import { Font } from 'expo'
-import { Ionicons } from '@expo/vector-icons'
-import styled from 'styled-components/native'
+import { AuthSession } from 'expo'
+// import { Ionicons } from '@expo/vector-icons'
+// import styled from 'styled-components/native'
 import React, { Component } from 'react'
+import {
+  Button as Button2, StyleSheet, Text as Text2, View
+} from 'react-native'
 import {
   Button,
   Container,
   Content,
-  Header,
+  // Header,
   Icon,
   Left,
   List,
@@ -15,6 +18,8 @@ import {
   Text,
   Thumbnail
 } from 'native-base'
+
+const FB_APP_ID = '434884557246474'
 
 export default class TEST extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -31,9 +36,45 @@ export default class TEST extends Component {
     )
   });
 
+  state = {
+    result: null
+  };
+
+  // isDataAccessExpired
+  // dataAccessExpirationTime
+  // payload: data_access_expiration_time
+
+  handlePressAsync = async() => {
+    const redirectUrl = AuthSession.getRedirectUrl()
+    const result = await AuthSession.startAsync({
+      authUrl:
+        `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
+        `&client_id=${FB_APP_ID}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}`
+    })
+    const profile = await this.getProfile(result.params.access_token)
+    const profileData = await profile.json()
+    console.log(profileData)
+    this.setState({ result, profileData })
+  };
+
+  getProfile = async token => (
+    fetch(`${'https://graph.facebook.com/v3.2/me?fields='}${['id', 'email', 'name', 'picture'].join(',')}&access_token=${token}`)
+  )
+
   render() {
+    const { result, profileData } = this.state
     return (
       <Container>
+        <View style={styles.container}>
+          <Button2 title='Continue with Facebook' onPress={this.handlePressAsync} />
+          {/* {result ? (
+            <Text2>{JSON.stringify(result)}</Text2>
+          ) : null} */}
+          {result ? (
+            <Text2>{JSON.stringify(profileData)}</Text2>
+          ) : null}
+        </View>
         <Content>
           <List>
             <ListItem first button>
@@ -56,3 +97,11 @@ export default class TEST extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
